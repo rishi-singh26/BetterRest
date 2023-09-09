eaf195d2dd00e86f7c0f515d686860d740048ab2
rishi-singh26 <rishisingh0831@gmail.com>
2023-09-09 08:45:26 +0530

    Dates notes

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepValue = 8.0
+    @State private var sleepDate = Date.now
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Stepper("\(sleepValue.formatted()) hours", value: $sleepValue, in: 4...12, step: 0.25)
+            DatePicker("Pick a date", selection: $sleepDate, in: Date.now...)
+            Text(Date.now, format: .dateTime.day().month().year())
+            Text(Date.now.formatted(date: .long, time: .shortened))
         }
         .padding()
+    }
+    
+    func dateExample() {
+        let component = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
+        let hour = component.hour ?? 0
+        let minute = component.minute ?? 0
+         
     }
 }
 

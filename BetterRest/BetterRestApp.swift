11a7d9c3b17df832b01663bf46c7a6dba07b7d52
rishi-singh26 <rishisingh0831@gmail.com>
2023-09-09 07:51:19 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Rishi Singh on 09/09/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
